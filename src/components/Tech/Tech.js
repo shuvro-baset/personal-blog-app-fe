2aee718d734f1a8c96aa2e0e8326cfe841434f81
shuvro baset <md.abdul.baset75@gmail.com>
2022-01-04 15:13:51 +0600

    d

--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -12,16 +12,16 @@ const Tech = () => {
         <Container>
             <h1 className="title-2"> I love New Technologies, Programming, Coding.</h1>
             <Row className="my-5">
-                <Col md={4}>
+                <Col md={4} data-aos="zoom-in-right">
                     <img className="img-fluid  rounded" src="https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
                 </Col>
                 <Col md={4}>
                     <div className="d-flex flex-column justify-content-center align-content-center">
-                        <img className="img-fluid my-2 rounded" src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-                        <img className="img-fluid my-2 rounded" src="https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <img data-aos="zoom-out-up" className="img-fluid my-2 rounded" src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                        <img data-aos="zoom-out-down" className="img-fluid my-2 rounded" src="https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
                     </div>
                 </Col>
-                <Col md={4}>
+                <Col md={4} data-aos="zoom-in-left">
                     <img className="img-fluid  rounded" src="https://images.unsplash.com/photo-1608306448197-e83633f1261c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
                 </Col>
             </Row>
